c574b6ab3606b734b0fa9ed88fa84a72356a63c9
giulio-ita <giuliomb89@gmail.com>
2025-02-20 14:05:50 +0100

    inital commit

---
+++ overlookManagement/src/main/java/com/overlookManagement/OverlookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.overlookManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlookManagementApplication.class, args);
+	}
+
+}
---
+++ overlookManagement/src/test/java/com/overlookManagement/OverlookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overlookManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
